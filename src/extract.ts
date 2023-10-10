a527b16a0def894631a7a625d4f864fb0e96f815
jill64 <intents.turrets0h@icloud.com>
2023-10-10 21:39:05 +0900

    ref: src

--- src/extract.ts
+++ src/extract.ts
@@ -1,18 +1,20 @@
-import { preparedSerializers } from './preparedSerializers.js'
+import type { MakeQparam } from './types/MakeQparam.js'
 import type { PreparedSerializerKey } from './types/PreparedSerializerKey.js'
 import type { Serializer } from './types/Serializer.js'
+import { isPreparedSerializerKey } from './utils/isPreparedSerializerKey.js'
+import { preparedSerializers } from './utils/preparedSerializers.js'
 
-export const extract = (url: string | URL) => {
+export const extract = (url: string | URL): MakeQparam => {
   const source = new URL(url)
 
-  return <T = string>(
+  return <T>(
     key: string,
-    serializer: Serializer<T> | PreparedSerializerKey = 'string'
+    serializer?: Serializer<T> | PreparedSerializerKey
   ) => {
     const { parse, stringify } = (
-      typeof serializer === 'string'
+      isPreparedSerializerKey(serializer)
         ? preparedSerializers[serializer]
-        : serializer
+        : serializer ?? preparedSerializers.string
     ) as Serializer<T>
 
     const get = () => {
