28147ce1ffe52cd56893c79e40b98c21a956a513
jill64 <intents.turrets0h@icloud.com>
2023-10-10 21:50:53 +0900

    improve coverage

---
+++ test/custom.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from 'vitest'
+import { extract } from '../src/index'
+
+test('custom', () => {
+  const qparam = extract('https://example.com/?json={"key": "value"}')
+  const json = qparam<{ key: string }>('json', {
+    stringify: (value) => JSON.stringify(value),
+    parse: (value) => JSON.parse(value ?? '')
+  })
+  expect(json.get()).toEqual({ key: 'value' })
+  expect(json.set({ key: 'new_value' }).href).toBe(
+    'https://example.com/?json=%7B%22key%22%3A%22new_value%22%7D'
+  )
+})
