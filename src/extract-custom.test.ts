4c5ff5d4a768c67c49770317cc52f44fd924b274
jill64 <intents.turrets0h@icloud.com>
2023-11-28 18:18:47 +0900

    BREAKING: refactor with ts-serde

--- src/extract-custom.test.ts
+++ src/extract-custom.test.ts
@@ -1,14 +1,43 @@
 import { expect, test } from 'vitest'
 import { extract } from './index.js'
+import { json } from './serde/index.js'
 
 test('custom', () => {
-  const qparam = extract('https://example.com/?json={"key": "value"}')
-  const json = qparam<{ key: string }>('json', {
-    stringify: (value) => JSON.stringify(value),
-    parse: (value) => JSON.parse(value ?? '')
-  })
-  expect(json.get()).toEqual({ key: 'value' })
-  expect(json.set({ key: 'new_value' }).href).toBe(
+  const qparam = extract(new URL('https://example.com/?json={"key": "value"}'))
+  const js = qparam(
+    'json',
+    json(
+      (x): x is { key: string } =>
+        !!(
+          x &&
+          typeof x === 'object' &&
+          'key' in x &&
+          typeof x.key === 'string'
+        )
+    )
+  )
+
+  expect(js.get()).toEqual({ key: 'value' })
+  expect(js.set({ key: 'new_value' }).href).toBe(
     'https://example.com/?json=%7B%22key%22%3A%22new_value%22%7D'
+  )
+})
+
+const isStringArray = (x: unknown): x is string[] =>
+  Array.isArray(x) && x.every((y) => typeof y === 'string')
+
+test('string-array', () => {
+  const qparam = extract(
+    new URL('https://example.com/?csv=["foo","bar","baz"]')
+  )
+
+  const non = qparam('non', json(isStringArray, []))
+  expect(non.get()).toEqual([])
+
+  const csv = qparam('csv', json(isStringArray))
+  expect(csv.get()).toEqual(['foo', 'bar', 'baz'])
+  expect(csv.set(['12', 'AB', '-=']).href).toBe(
+    new URL('https://example.com/?csv=%5B%2212%22%2C%22AB%22%2C%22-%3D%22%5D')
+      .href
   )
 })
--- src/utils/isPreparedSerializerKey.ts
+++
@@ -1,7 +0,0 @@
-import { PreparedSerializerKey } from '../types/PreparedSerializerKey.js'
-import { preparedSerializers } from './preparedSerializers.js'
-
-export const isPreparedSerializerKey = (
-  key: unknown
-): key is PreparedSerializerKey =>
-  typeof key === 'string' && key in preparedSerializers
